0821c1e726166c0adcf693d223bfcd323d2e25be
Diogo Nogueira <34174814+diogoesnog@users.noreply.github.com>
2019-11-21 15:51:54 +0000

    Eliminar persons.js.

--- WebApp/app/controllers/users.js
+++ WebApp/app/controllers/users.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const User = require('../models/user');
 
+// List Group of Users, depending on a Query
 module.exports.list = (query) => {
     return User.find(query).exec();
 };
 
+// Add new User to Database
 module.exports.addNew = (data) => {
     let newData = new User(data);
 
