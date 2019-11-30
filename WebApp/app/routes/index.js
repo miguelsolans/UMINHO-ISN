533c45bef775029946aa4504b9c910f21fc4935d
Miguel Solans <dev.miguelsolans@gmail.com>
2019-11-30 15:52:34 +0000

    Login

--- WebApp/app/routes/index.js
+++ WebApp/app/routes/index.js
@@ -2,20 +2,26 @@
 const express = require('express');
 const router = express.Router();
 
-// Controllers
-const Users = require('../controllers/users');
+
 
 router.get('/', (req, res) => {
-    Users.list()
-        .then(data => res.jsonp(data))
-        .catch(err => res.status(500).jsonp(err));
+    res.render('login');
 });
 
-router.post('/', (req, res) => {
-    Users.addNew(req.body)
-        .then(data => res.json(data))
-        .catch(err => res.status(500).jsonp(err));
-});
+// Controllers
+// const Users = require('../controllers/users');
+//
+// router.get('/', (req, res) => {
+//     Users.list()
+//         .then(data => res.jsonp(data))
+//         .catch(err => res.status(500).jsonp(err));
+// });
+//
+// router.post('/', (req, res) => {
+//     Users.addNew(req.body)
+//         .then(data => res.json(data))
+//         .catch(err => res.status(500).jsonp(err));
+// });
 
 // Export Routes for Index
 module.exports = router;
