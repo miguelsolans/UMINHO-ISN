dd148d28277154397f618059058fc9ecaf932b2b
Miguel Solans <dev.miguelsolans@gmail.com>
2019-11-30 23:24:10 +0000

    Mockups

--- WebApp/app/routes/index.js
+++ WebApp/app/routes/index.js
@@ -3,25 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 
-
 router.get('/', (req, res) => {
     res.render('login');
 });
 
-// Controllers
-// const Users = require('../controllers/users');
-//
-// router.get('/', (req, res) => {
-//     Users.list()
-//         .then(data => res.jsonp(data))
-//         .catch(err => res.status(500).jsonp(err));
-// });
-//
-// router.post('/', (req, res) => {
-//     Users.addNew(req.body)
-//         .then(data => res.json(data))
-//         .catch(err => res.status(500).jsonp(err));
-// });
 
 // Export Routes for Index
 module.exports = router;
