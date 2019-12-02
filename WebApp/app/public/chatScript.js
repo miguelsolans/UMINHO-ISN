c9f3e745624b9a89d3ad55ab6f72bdbf4cea43e9
Miguel Solans <dev.miguelsolans@gmail.com>
2019-12-02 17:55:31 +0000

    QuillJS

--- WebApp/app/public/chatScript.js
+++ WebApp/app/public/chatScript.js
@@ -1,8 +1,34 @@
-//TODO: Passar para a pasta modules e apagar depois de testar
+// TODO: Passar para a pasta modules e apagar depois de testar
 
 $(document).ready(() => {
-    var quill = new Quill('#editor', {
+    const quill = new Quill('#editor', {
         theme: 'snow',
-        placeholder: 'Reply message...',
+        placeholder: 'Reply to chat...',
+        formats: [
+            'bold',
+            'header',
+            'italic',
+            'link',
+            'list',
+            'blockquote',
+            'image',
+            'indent'
+        ],
+        modules: {
+            toolbar: [
+                ['bold', 'italic', 'link'],
+                ['blockquote', {'list': 'ordered'}, {'list': 'bullet'}],
+            ],
+            clipboard: {
+                matchVisual: false // https://quilljs.com/docs/modules/clipboard/#matchvisual
+            }
+        }
+    });
+
+    $('#send-message').click( () => {
+        let msgTxt =  quill.container.firstChild.innerHTML;
+        console.log(msgTxt);
+        $("#message1").append(msgTxt);
+        console.log(msgTxt);
     });
 });
