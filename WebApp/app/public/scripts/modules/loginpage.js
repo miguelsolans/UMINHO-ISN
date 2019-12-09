d9964552bdcc734ba38b8345080a32a3141c63ef
Miguel Solans <dev.miguelsolans@gmail.com>
2019-12-09 21:10:51 +0000

    Settings

--- WebApp/app/public/scripts/modules/loginpage.js
+++ WebApp/app/public/scripts/modules/loginpage.js
@@ -1,19 +1,12 @@
-define([
-    'jquery'
-], ($) => {
+$(document).ready(() => {
 
-
-   $(document).ready(() => {
-        $('#call-register').click( () => {
-            $('#register-form-container').show();
-            $('#login-form-container').hide(750);
-        });
-
-        $('#call-login').click( () => {
-            $('#register-form-container').hide(750);
-            $('#login-form-container').show();
-        })
-   });
-
+    $('#call-register').click( () => {
+        $('#register-form-container').show();
+        $('#login-form-container').hide(750);
+    });
+    $('#call-login').click( () => {
+        $('#register-form-container').hide(750);
+        $('#login-form-container').show();
+    });
 
 });
--- WebApp/app/routes/settings.js
+++ WebApp/app/routes/settings.js
@@ -2,8 +2,29 @@ const express = require('express');
 const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
 
+const User = require('../controllers/users');
+
 router.get('/', checkAuth, (req, res) => {
-    res.render('settings');
+
+    console.log(req.decodedUser);
+
+    User.searchUser(req.decodedUser)
+        .then(data => {
+            let fieldsToDisplay = {
+                _id: data._id,
+                fullName: data.fullName,
+                bio: data.bio,
+                email: data.email
+            };
+            res.render('settings', { data: data });
+            // res.render('settings', { data: {
+            //         _id: data._id,
+            //         fullName: data.fullName,
+            //         bio: data.bio,
+            //         email: data.email
+            //     }});
+        })
+        .catch(err => console.log(err));
 });
 
 module.exports = router;
