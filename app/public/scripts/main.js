6e42e9b4f002e9af8618a35216558d80e0ebf870
Miguel Solans <dev.miguelsolans@gmail.com>
2020-01-25 17:14:40 +0000

    RequireJS Integration

--- app/public/scripts/main.js
+++ app/public/scripts/main.js
@@ -1,19 +1,58 @@
 require.config({
     "paths": {
-        // External
-        "jquery": "external/jquery",
-        "bootstrap": "external/bootstrap",
 
-        // Modules
-        "loginPage": "modules/loginPage"
+        /**
+         * Application External Libraries
+         */
+        // jQuery & Plguins
+        "jquery": "external/jQuery/jquery-3.4.1.min",
+        "jquery-confirm": "external/jQuery/plugins/jquery-confirm",
+
+        // Bootstrap
+        "bootstrap": "external/bootstrap/bootstrap",
+        "popper": "external/bootstrap/popper",
+
+        // Tagify
+        "tagify": "external/tagify/tagify",
+
+
+        // QuillJS
+        "quill": "external/quill/quill",
+
+
+        /**
+         * Application Modules
+         */
+        // Settings
+        "settings": "modules/settings/settings",
+        // Messages
+        "messages": "modules/messages/messages",
+
+        /**
+         * Application Utils
+         */
+        // Notification Alerts
+        "alert": "modules/utils/alerts"
+
+    },
+
+    /**
+     * Shim Libraries
+     */
+    "jquery-confirm": {
+        deps: ['jquery'],
+        exports: 'confirm'
     },
     "bootstrap": {
         "deps": [
-            'jquery'
+            'jquery',
+            'popper'
         ]
     }
 });
 
 require([
-    'loginPage',
+    'settings',
+    'messages',
+    'alert'
 ], () => console.log("RequireJS Modules Loaded"));
