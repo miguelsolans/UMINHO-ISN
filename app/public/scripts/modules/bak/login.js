9707d711b4c83de78f717ca8fc28f3a0872b92b0
Miguel Solans <dev.miguelsolans@gmail.com>
2020-01-25 18:19:33 +0000

    RequireJS and Libraries (#41)
    * Group Posts API
    
    * User Feed Files Upload
    
    * RequireJS Integration
    
    * RequireJS Bug Fixes
    
    * Scripts

---
+++ app/public/scripts/modules/bak/login.js
@@ -0,0 +1,13 @@
+import * as jQuery from '../external/jquery.js';
+
+
+$(document).ready(() => {
+    $('#call-register').click( () => {
+        $('#register-form-container').show();
+        $('#login-form-container').hide(750);
+    });
+    $('#call-login').click( () => {
+        $('#register-form-container').hide(750);
+        $('#login-form-container').show();
+    });
+});
---
+++ app/public/scripts/modules/bak/settings.js
@@ -0,0 +1,92 @@
+$(document).ready(() => {
+    let input = document.querySelector("input[name=courses]");
+    let tagify = new Tagify(input);
+
+    // Triggers
+    // Edit Profile Picture Sidebar
+    $('#edit-profile').on('click', event => {
+        $('#editProfile').modal('show');
+    });
+    // Edit Password
+    $('#change-password').on('click', event => {
+        $('#changePasswordForm').modal('show');
+    });
+
+    // Perform Put Actions
+    $('#changePasswordButton').on('click', event => {
+        let newPassword = $('#password').val();
+
+        $.ajax({
+            type: "PUT",
+            url: 'http://localhost:3030/settings/change-password',
+            data: `password=${newPassword}`,
+            success: result => {
+                console.log(result);
+            },
+            error: result => {
+                console.log(result);
+                alert(result);
+            }
+        })
+    });
+
+
+    /**
+     * QuillJS Configuration
+     */
+    const quill = new Quill('#quill-settings-about', {
+        theme: 'snow',
+        placeholder: 'Write something cool 😎',
+        formats: [
+            'bold',
+            'header',
+            'italic',
+            'link',
+            'list',
+            'blockquote',
+            'image',
+            'indent'
+        ],
+        modules: {
+            toolbar: [
+                ['bold', 'italic', 'link'],
+                ['blockquote', {'list': 'ordered'}, {'list': 'bullet'}],
+            ],
+            clipboard: {
+                matchVisual: false // https://quilljs.com/docs/modules/clipboard/#matchvisual
+            }
+        }
+    });
+
+
+    // Submit Changes
+    const $infoForm = $('#update-user-info');
+    $infoForm.on('submit', handler => {
+        handler.preventDefault();
+
+        let content = $("#quill-settings-about .ql-editor").html();
+
+        $("#bio").attr("value", content);
+
+        let dataArray = $infoForm.serializeArray();
+        let dataJson = {};
+        dataArray.forEach(entry => {
+            dataJson[entry.name] = entry.value;
+        });
+
+        console.log(dataJson);
+        $.ajax({
+            url: `/api/settings/update`,
+            type: 'put',
+            data: dataJson,
+            success: response => console.log(response),
+            error: response => console.log(response)
+        });
+    });
+
+
+
+    $('#signout').on('click', () => {
+        document.cookie = "userToken" + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    });
+});
