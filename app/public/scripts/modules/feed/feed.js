138af74ef7e2ae4c364d976a77d12584cd25a7db
Miguel Solans <dev.miguelsolans@gmail.com>
2020-01-26 19:06:08 +0000

    Fixed broken ids on post comments

--- app/public/scripts/modules/feed/feed.js
+++ app/public/scripts/modules/feed/feed.js
@@ -54,7 +54,7 @@ define([
             error: response => console.log(response)
         });
     });
-    
+
     $(document).ready(() => {
         /**
          * View Comments Modal
@@ -66,6 +66,9 @@ define([
                 url: `/api/userpost/comments/${id}`,
                 success: response => {
                     $('#comments-container').html("");
+
+                    $('#submit-post-comment').attr('action', `api/userpost/comment/${id}`);
+
                     if (response.length > 0) {
                         response[0].Comments.forEach(content => formatComment(content));
                     }
@@ -86,6 +89,7 @@ define([
                 method: "GET",
                 url: `/api/userpost/${id}`,
                 success: response => {
+
                     $('#edit-post-text').val(response.content.text);
                     $('#edit-post-id').attr('value', response._id);
                     $('#edit-post-modal').modal()
@@ -158,23 +162,24 @@ define([
 
             $postForm.unbind('submit').submit();
 
-        })
-    });
+        });
+
+        // Change Post form to Display uploader
+        // $('#add-post-files').keypress(function(e) {
+        //     var keycode = (e.keyCode ? e.keyCode : e.which);
+        //     console.log("pressed");
+        //     if(keycode == '13') {
+        //         if($('#add-post-files').is(':checked')) {
+        //             console.log("checked");
+        //         }
+        //     }
+        // });
+
+        // Highlight
+        document.querySelectorAll('pre .ql-syntax').forEach((block) => {
+            highlight.highlightBlock(block);
+        });
 
 
-    // Change Post form to Display uploader
-    $('#add-post-files').keypress(function(e) {
-        var keycode = (e.keyCode ? e.keyCode : e.which);
-        console.log("pressed");
-        if(keycode == '13') {
-            if($('#add-post-files').is(':checked')) {
-                console.log("checked");
-            }
-        }
-    });
-
-    // Highlight
-    document.querySelectorAll('pre .ql-syntax').forEach((block) => {
-        highlight.highlightBlock(block);
     });
 });
--- app/routes/user-posts.js
+++ app/routes/user-posts.js
@@ -5,6 +5,7 @@ const checkAuth = require('../middleware/check-auth');
 const fs = require('fs');
 const multer = require('multer');
 const path = require('path');
+const axios = require('axios');
 
 const upload = multer({
     dest: 'app/public/uploads'
@@ -46,6 +47,21 @@ router.post('/', checkAuth, upload.array('files', 12), (req, res) => {
     Post.addNew(newPost)
         .then(result => res.redirect('/feed'))
         .catch(err => res.jsonp(err));
+
+});
+
+
+router.post('/comment/:postId', checkAuth, (req, res) => {
+    console.log("Requested Comment Post");
+
+    axios(`${process.env.API_URL}/userpost/comment/${req.params.postId}`, {
+        method: "post",
+        data: req.body,
+        headers: {
+            Cookie: `userToken=${req.cookies.userToken}`
+        },
+    }).then(response => res.redirect('/feed'))
+        .catch(err => console.log(err));
 
 });
 
