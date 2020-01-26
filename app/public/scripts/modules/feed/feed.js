f0d888cb06c05ed442b1a3108d1282876efe05cc
Mateus Silva <36518616+mateussilva98@users.noreply.github.com>
2020-01-26 14:25:24 +0000

    deus ouviu-nos :') (#52)
    * api user posts & comments
    
    adicionei a api das publicações, incluindo a parte dos comentários. para além disso, foi feito o middleware que verifica se o utilizador autenticado pode remover e atualizar publicações ou se pode remover ou atualizar comentários
    
    * group posts & other stuff
    
    * authhhhh sem cockies depois do axios
    
    * Fixed User Token
    
    * back end feed done
    
    * foto
    
    * seeder group
    
    * groups
    
    * tempo passado desde post
    
    * ://///
    
    * resolver problemas
    
    * jquery posts
    
    * obrigado deus miguel
    
    * login
    
    * Delete Posts
    
    * api get single post by is
    
    * Delete and Edit Posts
    
    * Fixed a Bug where /api/users stopped Working
    
    * dados passados para profile
    
    deus ouviu-me
    
    Co-authored-by: Miguel Solans <dev.miguelsolans@gmail.com>
    Co-authored-by: Diogo Nogueira <34174814+diogoesnog@users.noreply.github.com>

--- app/public/scripts/modules/feed/feed.js
+++ app/public/scripts/modules/feed/feed.js
@@ -1,153 +1,142 @@
 define([
-  'jquery',
-  'alert',
-  'composer',
-  'jquery-confirm',
-  'bootstrap'
+    'jquery',
+    'alert',
+    'composer',
+    'jquery-confirm',
+    'bootstrap'
 ], function ($, alert, composer) {
+    'use strict';
 
-'use strict';
-
-function formatComment(content) {
-  let info = `<div class="post-info border-bottom">
-    <div class="row">
-        <div class="col-2 p-3">
-            <div class="circle-avatar"
-                style="background-image:url(${content.InfoComment[0].photo})"></div>
-        </div>
-        <div class="col-8">
-            <t class="font-weight-bold">
-                ${content.InfoComment[0].fullName}
-            </t>
-            <t class="text-muted">
-            @${content.createdBy}
-            </t>
-            <br>
-            <div class="text-justify">
-               ${content.text}
+    function formatComment(content) {
+        let info = `<div class="post-info border-bottom">
+        <div class="row">
+            <div class="col-2 p-3">
+                <div class="circle-avatar"
+                    style="background-image:url(${content.InfoComment[0].photo})"></div>
+            </div>
+            <div class="col-8">
+                <t class="font-weight-bold">
+                    ${content.InfoComment[0].fullName}
+                </t>
+                <t class="text-muted">
+                @${content.username}
+                </t>
+                <br>
+                <div class="text-justify">
+                   ${content.text}
+                </div>
             </div>
         </div>
-    </div>
-</div>`;
+    </div>`;
 
-  $('#comments-container').append(info);
+        $('#comments-container').append(info);
 
-}
+    }
+    
+    
+    $(document).ready(() => {
+        /**
+         * View Comments Modal
+         */
+        $(document).on("click", ".view-comments", event => {
+            let id = $(event.currentTarget).attr('data-value');
+            $.ajax({
+                type: "get",
+                url: `/api/userpost/comments/${id}`,
+                success: response => {
+                    $('#comments-container').html("");
+                    if (response.length > 0) {
+                        response[0].Comments.forEach(content => formatComment(content));
+                    }
+                    $('#comments-modal').modal();
 
-$(document).ready(() => {
-  /**
-   * View Comments Modal
-   */
-  $(document).on("click", ".view-comments", event => {
-    let id = $(event.currentTarget).attr('data-value');
-    $.ajax({
-      type: "get",
-      url: `/api/userpost/comments/${id}`,
-      success: response => {
-        $('#comments-container').html("");
-        if (response.length > 0) {
-          response[0].Comments.forEach(content => formatComment(content));
-        }
-        $('#comments-modal').modal();
+                },
+                error: response => {
 
-      },
-      error: response => {
+                }
+            })
+        });
+        /**
+         * Edit a post
+         */
+        $(document).on('click', '.edit-post', event => {
 
-      }
-    })
-  });
-  /**
-   * Edit a post
-   */
-  $(document).on('click', '.edit-post', event => {
+            let id = $(event.currentTarget).attr("data-value");
 
-    let id = $(event.currentTarget).attr("data-value");
+            $.ajax({
+                method: "GET",
+                url: `/api/userpost/${id}`,
+                success: response => {
+                    $('#edit-post-text').val(response.content.text);
+                    $('#edit-post-id').attr('value', response._id);
+                    $('#edit-post-modal').modal()
+                },
+                error: response => console.log(response)
+            });
+        });
 
-    $.ajax({
-      method: "GET",
-      url: `/api/userpost/${id}`,
-      success: response => {
-        $('#edit-post-text').val(response.content.text);
-        $('#edit-post-id').attr('value', response._id);
-        $('#edit-post-modal').modal()
-      },
-      error: response => console.log(response)
+        // update-post-button
+        $('#update-post-button').on('click', () => {
+            let text = $('#edit-post-text').val();
+            let id = $('#edit-post-id').attr('value');
+
+            $.ajax({
+                method: "PUT",
+                url: `/api/userpost/${id}`,
+                data: {
+                    content: {
+                        text: text
+                    }
+                },
+                success: response => console.log(response),
+                error: response => console.log(response)
+            });
+        });
+
+        /**
+         * Delete a Given Post
+         */
+        $(document).on("click", ".delete-post", event => {
+            let id = $(event.currentTarget).attr('data-value');
+            $.confirm({
+                title: 'Remove Post?',
+                content: 'Are you sure!?',
+                buttons: {
+                    confirm: () => {
+                        $.alert('You removed your post...! 😢');
+
+                        $.ajax({
+                            type: "delete",
+                            url: `/api/userpost/${id}`,
+                            success: response => {
+                                alert.warningAlert({title: "Post Deleted", body: "You removed your own post"});
+                                console.log(response);
+                                $(`#${response._id}`).remove();
+                            },
+                            error: response => {
+                                console.log(response);
+                            }
+                        });
+                    },
+                    cancel: () => {
+                        $.alert('You opt-out not removing your kick-ass post 🤘🏽');
+                    },
+                }
+            });
+        });
+
+        /**
+         * Create a new Post
+         */
+        const $postForm = $('#feed-post-form');
+
+        $postForm.on('submit', () => {
+
+
+            let content = $(".quill-composer .ql-editor").html();
+            $('#user-post-text').attr('value', content);
+
+            $postForm.unbind('submit').submit();
+        })
     });
-  });
-
-          $.ajax({
-              method: "GET",
-              url: `/api/userpost/${id}`,
-              success: response => {
-                  $('#edit-post-text').val(response.content.text);
-                  $('#edit-post-id').attr('value', response._id);
-                  $('#edit-post-modal').modal()
-              },
-              error: response => console.log(response)
-          });
-      });
-  // update-post-button
-  $('#update-post-button').on('click', () => {
-    let text = $('#edit-post-text').val();
-    let id = $('#edit-post-id').attr('value');
-
-    $.ajax({
-      method: "PUT",
-      url: `/api/userpost/${id}`,
-      data: {
-        content: {
-          text: text
-        }
-      },
-      success: response => console.log(response),
-      error: response => console.log(response)
-    });
-  });
-
-  /**
-   * Delete a Given Post
-   */
-  $(document).on("click", ".delete-post", event => {
-    let id = $(event.currentTarget).attr('data-value');
-    $.confirm({
-      title: 'Remove Post?',
-      content: 'Are you sure?',
-      buttons: {
-        confirm: () => {
-          $.alert('You removed your Post.');
-
-          $.ajax({
-            type: "delete",
-            url: `/api/userpost/${id}`,
-            success: response => {
-              alert.warningAlert({
-                title: "Post Deleted",
-                body: "You removed your Post."
-              });
-              console.log(response);
-              $(`#${response._id}`).remove();
-            },
-            error: response => {
-              console.log(response);
-            }
-          });
-        },
-        cancel: () => {},
-      }
-    });
-  });
-
-  /**
-   * Create a new Post
-   */
-  const $postForm = $('#feed-post-form');
-
-  $postForm.on('submit', () => {
-
-    let content = $(".quill-composer .ql-editor").html();
-    $('#user-post-text').attr('value', content);
-
-
-    $postForm.unbind('submit').submit();
-  })
 });
